5850e5d99f94c8cbefc4179a2b5ccd109851cda8
Erick Merchant <erickmerchant@gmail.com>
2017-11-29 22:06:51 -0800

    Remove all options

--- main.js
+++ main.js
@@ -17,17 +17,6 @@ command('optimize', ({option, parameter}) => {
     required: true
   })
 
-  parameter('css', {
-    description: 'the file where your css is',
-    required: true
-  })
-
-  option('inline', {
-    description: 'inline the css and source map',
-    type: Boolean,
-    default: false
-  })
-
   return (args) => {
     const source = path.join(process.cwd(), args.source, '**/*.html')
 
@@ -36,103 +25,72 @@ command('optimize', ({option, parameter}) => {
       return Promise.all(files.map((file) => {
         return readFile(file, 'utf-8')
         .then((content) => {
+          const dom = new JSDOM(content)
+          const hrefs = [...dom.window.document.querySelectorAll('link[rel=stylesheet]')].map((el) => path.join(args.source, el.getAttribute('href')))
+
           return {
-            path: file,
-            dom: new JSDOM(content),
-            content
+            dom,
+            hrefs
           }
         })
       }))
     })
     .then((files) => {
-      return unstyle(files)
-      .then((output) => {
-        let result
+      return files.reduce((hrefs, file) => hrefs.concat(file.hrefs.filter((href, index) => file.hrefs.indexOf(href) === index)), []).map((href) => {
+        return Promise.all([
+          readFile(href, 'utf-8'),
+          readFile(href + '.map', 'utf-8')
+        ])
+        .then(([css, map]) => {
+          const plugins = [
+            postcss.plugin('optimize', (opts) => {
+              return (root, result) => {
+                root.walkRules(rule => {
+                  if (rule.parent.type === 'atrule' && rule.parent.name.endsWith('keyframes')) return
 
-        if (args.inline) {
-          result = Promise.all(files.map((file) => {
-            return inline(file, output)
-          }))
-        } else {
-          result = Promise.all([
-            writeFile(path.join(process.cwd(), `${args.css}`), String(output.css)),
-            writeFile(path.join(process.cwd(), `${args.css}.map`), output.map)
-          ])
-        }
+                  const selector = rule.selectors
+                    .map((selector) => selector.trim())
+                    .filter((selector) => {
+                      return files.reduce((isUsed, file) => {
+                        const stripped = stripPseudos(selector)
 
-        return result
-      })
-    })
+                        if (!stripped) return true
 
-    function unstyle (files) {
-      return Promise.all([
-        readFile(args.css, 'utf-8'),
-        readFile(args.css + '.map', 'utf-8')
-      ])
-      .then(([css, map]) => {
-        const plugins = [
-          postcss.plugin('optimize', function (opts) {
-            return function (root, result) {
-              root.walkRules(rule => {
-                if (rule.parent.type === 'atrule' && rule.parent.name.endsWith('keyframes')) return
+                        return file.dom.window.document.querySelector(stripped) != null ? true : isUsed
+                      }, false)
+                    })
+                    .join(', ')
 
-                const selector = rule.selectors
-                  .map((selector) => selector.trim())
-                  .filter((selector) => {
-                    return files.reduce((isUsed, file) => {
-                      const stripped = stripPseudos(selector)
+                  if (selector === '') {
+                    rule.remove()
+                  } else {
+                    rule.selector = selector
+                  }
+                })
+              }
+            })
+          ]
 
-                      if (!stripped) return true
+          const prev = JSON.parse(map)
 
-                      return file.dom.window.document.querySelector(stripped) != null ? true : isUsed
-                    }, false)
-                  })
-                  .join(', ')
+          prev.sources = prev.sources.map((source) => path.relative(process.cwd(), source))
 
-                if (selector === '') {
-                  rule.remove()
-                } else {
-                  rule.selector = selector
-                }
-              })
+          return postcss(plugins).process(css, {
+            from: '/' + path.relative(args.source, href),
+            to: '/' + path.relative(args.source, href),
+            map: {
+              prev,
+              annotation: `/${path.relative(args.source, href)}.map`
             }
           })
-        ]
-
-        const prev = JSON.parse(map)
-
-        prev.sources = prev.sources.map((source) => path.relative(process.cwd(), source))
-
-        return postcss(plugins).process(css, {
-          from: '/' + path.relative(args.source, args.css),
-          to: '/' + path.relative(args.source, args.css),
-          map: {
-            prev,
-            inline: args.inline,
-            annotation: `/${path.relative(args.source, args.css)}.map`
-          }
+        })
+        .then((output) => {
+          return Promise.all([
+            writeFile(path.join(process.cwd(), `${href}`), String(output.css)),
+            writeFile(path.join(process.cwd(), `${href}.map`), output.map)
+          ])
         })
       })
-    }
-
-    function inline (file, output) {
-      const dom = new JSDOM(file.content)
-
-      const css = String(output.css)
-
-      const href = path.relative(args.source, args.css)
-
-      let element = dom.window.document.querySelector('link[href$="' + href + '"]')
-
-      if (element == null) {
-        throw new Error('no link found')
-      }
-
-      const fragment = new JSDOM('<style type="text/css">' + css + '</style>')
-
-      element.parentNode.replaceChild(fragment.window.document.head.childNodes[0], element)
-
-      return writeFile(file.path, dom.serialize())
-    }
+    })
   }
 })(process.argv.slice(2))
